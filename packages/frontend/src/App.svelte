65930804c13e3393f1fc34fe637edd91e0fb986f
ookimaki <noreply@ookimaki.com>
2021-03-31 17:23:08 +0900

    added frontend

---
+++ packages/frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<p class="text-red-200">Hello {name}!</p>
+</main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>

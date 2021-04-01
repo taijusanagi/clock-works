c5b36b0070bc0e7c8e61e70a89dacf571103b0a0
ookimaki <noreply@ookimaki.com>
2021-04-01 12:03:33 +0900

    removed build bug

--- packages/frontend/src/components/organisms/a-clockwork-girl.svelte
+++ packages/frontend/src/components/organisms/a-clockwork-girl.svelte
@@ -1,6 +1,6 @@
 <div class="p-8">
   <a href="https://testnets.opensea.io/assets/0xd5e2bf046b30e141d5580bab3e133547377e58b8/1" target="_blank">
-    <img src="/img/doll.png" class="cursor-pointer w-48 mx-auto mb-3 shadow" />
+    <img src="/img/doll.png" alt="a-clockwork-girl" class="cursor-pointer w-48 mx-auto mb-3 shadow" />
   </a>
   <div class="border w-48 mx-auto p-1 rounded-md shadow">
     <p class="text-center text-xs font-medium text-gray-400 mb-1">A Clockwork Girl</p>
--- packages/frontend/src/components/organisms/hero.svelte
+++ packages/frontend/src/components/organisms/hero.svelte
@@ -1,6 +1,6 @@
 <div class="relative">
   <div class="absolute flex w-full">
-    <img src="/img/logo.png" class="h-32 w-32 opacity-5 mx-auto" />
+    <img src="/img/logo.png" alt="logo" class="h-32 w-32 opacity-5 mx-auto" />
   </div>
   <div class="mt-16">
     <p class="text-center text-3xl text-gray-800 tracking-widest mb-2">clockworks</p>
